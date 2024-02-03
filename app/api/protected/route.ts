87de845e8e325462c39a7d9aa4f87f92aece53a3
Gaurav Rana <gaurav.ranaji96@gmail.com>
2024-02-03 12:41:12 +0530

    ts fix

--- app/api/protected/route.ts
+++ app/api/protected/route.ts
@@ -1,8 +1,9 @@
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
-import { NextResponse } from 'next/server';
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import { NextResponse } from "next/server";
 
 export const GET = withApiAuthRequired(async function myApiRoute(req) {
   const res = new NextResponse();
-  const { user } = await getSession(req, res);
-  return NextResponse.json({ protected: 'My Secret', id: user.sub }, res);
+  const session = await getSession(req, res);
+  const user = session;
+  return NextResponse.json({ protected: "My Secret", id: user?.sub }, res);
 });
