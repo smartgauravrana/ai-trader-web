ac3ba85be52741a39a4ed9c14d84b79b35a5d219
Gaurav Rana <gaurav.ranaji96@gmail.com>
2024-02-03 17:12:00 +0530

    export removed for fontSans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
 // import "../styles/styles.css";
 import React, { PropsWithChildren } from "react";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 // import { PageLayout } from "@/components/page-layout";
-import './globals.css'
+import "./globals.css";
 
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import PageHeader from "@/components/page-header";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
-
+});
 
 // export const metadata = siteMetadata;
-
 
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en">
       {/* <PreloadResources /> */}
-      <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable
-      )} >
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <UserProvider>
           <PageHeader />
           <div>{children}</div>
