a95672f98aa09ad62412e056f16ca97aba52054b
Gaurav Rana <gaurav.ranaji96@gmail.com>
2024-02-04 09:08:22 +0530

    cache removed from api

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,5 +1,7 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
+export const dynamic = "force-dynamic";
+
 export const GET = handleAuth({
   login: handleLogin({
     returnTo: "/",
