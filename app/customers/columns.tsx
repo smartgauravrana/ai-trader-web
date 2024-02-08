f7e9cb4de09f9cc67fb6ee0fea4c448bf767c18e
Gaurav Rana <gaurav.ranaji96@gmail.com>
2024-02-08 23:30:04 +0530

    fixes

--- app/customers/columns.tsx
+++ app/customers/columns.tsx
@@ -1,68 +1,68 @@
-"use client";
+// "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { Customer } from "@/models/Customer";
+// import { ColumnDef } from "@tanstack/react-table";
+// import { MoreHorizontal } from "lucide-react";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuLabel,
+//   DropdownMenuSeparator,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
+// import { Button } from "@/components/ui/button";
+// import { Customer } from "@/models/Customer";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
+// // This type is used to define the shape of our data.
+// // You can use a Zod schema here if you want.
+// export type Payment = {
+//   id: string;
+//   amount: number;
+//   status: "pending" | "processing" | "success" | "failed";
+//   email: string;
+// };
 
-export const columns: ColumnDef<Customer>[] = [
-  {
-    accessorKey: "name",
-    header: "Name",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    accessorKey: "phone",
-    header: "Phone",
-  },
-  {
-    id: "actions",
-    header: "Actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const customer = row.original;
+// export const columns: ColumnDef<Customer>[] = [
+//   {
+//     accessorKey: "name",
+//     header: "Name",
+//   },
+//   {
+//     accessorKey: "email",
+//     header: "Email",
+//   },
+//   {
+//     accessorKey: "phone",
+//     header: "Phone",
+//   },
+//   {
+//     id: "actions",
+//     header: "Actions",
+//     enableHiding: false,
+//     cell: ({ row }) => {
+//       const customer = row.original;
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(customer.id)}
-            >
-              Copy Phone
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>Edit customer</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
+//       return (
+//         <DropdownMenu>
+//           <DropdownMenuTrigger asChild>
+//             <Button variant="ghost" className="h-8 w-8 p-0">
+//               <span className="sr-only">Open menu</span>
+//               <MoreHorizontal className="h-4 w-4" />
+//             </Button>
+//           </DropdownMenuTrigger>
+//           <DropdownMenuContent align="end">
+//             <DropdownMenuLabel>Actions</DropdownMenuLabel>
+//             <DropdownMenuItem
+//               onClick={() => navigator.clipboard.writeText(customer.id)}
+//             >
+//               Copy Phone
+//             </DropdownMenuItem>
+//             <DropdownMenuSeparator />
+//             <DropdownMenuItem>View customer</DropdownMenuItem>
+//             <DropdownMenuItem>Edit customer</DropdownMenuItem>
+//           </DropdownMenuContent>
+//         </DropdownMenu>
+//       );
+//     },
+//   },
+// ];
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,22 +1,7 @@
-'use client';
+"use client";
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function ProfileClient() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  return (
-    user && (
-      <div>
-        <img src={user.picture || ""} alt={user.name || ""} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <Link href={"/customers"}>Customers</Link>
-      </div>
-    )
-  );
+  return <div>Profile Page soming soon..</div>;
 }
